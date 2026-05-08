9bcbf626d6b402995d219d9b551f9b1bc4114321
grml <grml@grml.grml>
2026-05-08 15:15:27 +0800

    chore: added explanations and details

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,10 +1,14 @@
 const App = () => {
+	// regular javascript that gets executed in the browser
 	const now = new Date();
 	const a = 10;
 	const b = 20;
 	console.log(now, a + b);
 
-	return  (
+	// javascript in the curly braces are embedded upon evaluation
+
+	// () with a return statement is done when markup spans multiple lines
+	return (
 		<div>
 			<p>
 				Hello world, it is {now.toString()}
@@ -18,3 +22,25 @@ const App = () => {
 }
 
 export default App
+
+
+// the following is the transpiled version of the above, which will also work in jsx. This is typically handled by babel
+
+// const App = () => {
+// 	const now = new Date()
+// 	const a = 10
+// 	const b = 20
+// 	return React.createElement(
+// 		'div',
+// 		null,
+// 		React.createElement(
+// 			'p', null, 'Hello world, it is ', now.toString()
+// 		),
+// 		React.createElement(
+// 			'p', null, a, ' plus ', b, ' is ', a + b
+// 		)
+// 	)
+//   }
+
+// jsx is more xml like, so every element requires a closing tag, e.g. <br /> <img />
+
