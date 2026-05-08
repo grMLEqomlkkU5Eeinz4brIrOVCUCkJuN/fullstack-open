1eed99d4531c712b6dd9f86fb5f3db708a36c590
grml <grml@grml.grml>
2026-05-08 16:42:28 +0800

    feat: 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,46 @@
+const Header = ({ course }) => {
+	return <h1>{course.name}</h1>
+}
+
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Content = ({ course }) => {
+	const content = course.parts.map(part => {
+		return <Part name={part.part} exercises={part.exercises}/>
+	});
+	return <div><p>{content}</p></div>
+}
+
+const Total = ({ course }) => {
+	const total = course.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
+	return <p>{total}</p>
+}
+
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				part: "Fundamentals of React",
+				exercises: 10
+			},
+			{
+				part: "Using props to pass data",
+				exercises: 7
+			},
+			{
+				part: "State of a component'",
+				exercises: 14
+			},
+		]
+	}
 
 	return (
 		<div>
-			<h1>{course}</h1>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
-			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+			<Header course={course} />
+			<Content course={course} />
+			<Total course={course} />
 		</div>
 	)
 }
