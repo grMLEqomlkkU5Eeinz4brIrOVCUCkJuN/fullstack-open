d05c47a2d155f082aec3735c3d1164e97016994a
grml <grml@grml.grml>
2026-05-08 15:19:19 +0800

    feat: working with multiple components

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,27 +1,50 @@
-const App = () => {
-	// regular javascript that gets executed in the browser
-	const now = new Date();
-	const a = 10;
-	const b = 20;
-	console.log(now, a + b);
-
-	// javascript in the curly braces are embedded upon evaluation
-
-	// () with a return statement is done when markup spans multiple lines
+const Hello = () => {
 	return (
 		<div>
-			<p>
-				Hello world, it is {now.toString()}
-			</p>
+			<p>Hello World</p>
+		</div>
+	)
+}
 
-			<p>
-				{a} plus {b} is {a + b}
-			</p>
+const App = () => {
+	return (
+		<div>
+			<h1>Greetings</h1>
+			<Hello />
+			<Hello />
+			<Hello />
+			<Hello />
 		</div>
 	)
 }
 
 export default App
+
+
+
+// const App = () => {
+// 	// regular javascript that gets executed in the browser
+// 	const now = new Date();
+// 	const a = 10;
+// 	const b = 20;
+// 	console.log(now, a + b);
+
+// 	// javascript in the curly braces are embedded upon evaluation
+
+// 	// () with a return statement is done when markup spans multiple lines
+// 	return (
+// 		<div>
+// 			<p>
+// 				Hello world, it is {now.toString()}
+// 			</p>
+
+// 			<p>
+// 				{a} plus {b} is {a + b}
+// 			</p>
+// 		</div>
+// 	)
+// }
+
 
 
 // the following is the transpiled version of the above, which will also work in jsx. This is typically handled by babel
