35d2510c8ae85445cc4c89e3a1263ec706f78708
grml <grml@grml.grml>
2026-05-11 15:23:57 +0800

    fix: moved definitions of components outside of app. Refactored for step 5 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
-const App = () => {
-	// save clicks of each button to its own state
-	const [good, setGood] = useState(0)
-	const [neutral, setNeutral] = useState(0)
-	const [bad, setBad] = useState(0)
 
+	const Button = ({ label, onClick }) => {
+		return <button onClick={onClick}>{label}</button>
+	}
+
+	const StatisticLine = ({ label, value }) => {
+		return <div>{label} {value}</div>
+	}
 
 	const Statistics = ({ good, bad, neutral }) => {
 		const total = good + bad + neutral || 0;
@@ -13,27 +15,34 @@ const App = () => {
 		if (total === 0) return <div>No feedback given</div>
 		return (
 			<div>
-				<div>average: { (good - bad) / (total) || 0 }</div>
-				<div>positive: { (good) / (total) * 100 || 0 }</div>
-				<div>total: { total || 0 }</div>
+				<StatisticLine label="average" value={(good - bad) / total || 0} />
+				<StatisticLine label="positive" value={(good / total * 100) || 0} />
+				<StatisticLine label="total" value={total || 0} />
 			</div>
 		)
 	}
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
 
 	return (
 		<div>
 			<h1>Give Feedback</h1>
 
-			<button onClick={() => setGood(good + 1)}>Good</button>
-			<button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-			<button onClick={() => setBad(bad + 1)}>Bad</button>
+			<Button label="Good" onClick={() => setGood(good + 1)} />
+			<Button label="Neutral" onClick={() => setNeutral(neutral + 1)} />
+			<Button label="Bad" onClick={() => setBad(bad + 1)} />
 
 			<h1>Statistics</h1>
 
-			<div>good {good}</div>
-			<div>neutral {neutral}</div>
-			<div>bad {bad}</div>
-			<Statistics />
+			<StatisticLine label="good" value={good} />
+			<StatisticLine label="neutral" value={neutral} />
+			<StatisticLine label="bad" value={bad} />
+
+			<Statistics good={good} bad={bad} neutral={neutral} />
 		</div>
 	)
 }
