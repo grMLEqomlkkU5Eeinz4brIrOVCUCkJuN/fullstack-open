3348dabc95635626529c75b5202247422956441c
grml <grml@grml.grml>
2026-05-19 18:30:57 +0800

    feat: added phone number storage support to the system
    refactor: refactored the checkDuplicates function

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,37 +1,45 @@
 import { useState } from 'react'
 
-const PersonComponent = ({ name }) => {
-	return <div>{name}</div>
+const PersonComponent = ({ name, number }) => {
+	return <div>{name} {number}</div>
 }
 
 const PeopleCollection = ({ persons }) => {
-	return <div>{persons.map(person => <PersonComponent key={person.id} name={person.name} />)}</div>
+	return <div>{persons.map(person => <PersonComponent key={person.id} name={person.name} number={person.number}/>)}</div>
 }
 
-const checkDuplicates = (nameToAdd, array) => {
-	return array.some((element) => nameToAdd === element.name );
+const checkDuplicates = (item, array, space) => {
+	// such a weird thing to do
+	return array.some((element) => item === element[`${space}`]);
 }
 
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', id: 0 }
+		{ name: 'Arto Hellas', id: 0, number: "040-1234567" }
 	])
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState("");
 
 	const addName = (event) => {
 		event.preventDefault();
-		if (checkDuplicates(newName, persons)) {
+		if (checkDuplicates(newName, persons, "name")) {
 			return alert(`${newName} is already added to phonebook`);
 			setNewName("");
+			setNewNumber("");
 		} else {
-			setPersons(persons.concat({ name: newName, id: persons.length }));
+			setPersons(persons.concat({ name: newName, id: persons.length, number: newNumber }));
+			setNewNumber("");
 			setNewName("");
 		}
 	}
 
 	const handleNameChange = (event) => {
-		setNewName(event.target.value)
+		setNewName(event.target.value);
+	}
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
 	}
 
 	return (
@@ -41,6 +49,10 @@ const App = () => {
 				<div>
 					name: <input value={newName}
 						onChange={handleNameChange} />
+				</div>
+				<div>
+					number: <input value={newNumber}
+						onChange={handleNumberChange} />
 				</div>
 
 				<div><button type="submit">add</button></div>
