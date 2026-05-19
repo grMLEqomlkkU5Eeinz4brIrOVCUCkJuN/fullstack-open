42bd46d0b84cbffa6523827359f058642d58ed0d
grml <grml@grml.grml>
2026-05-19 15:13:42 +0800

    feat: added buttons, collections handler and proper components for rending updated output

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,27 +1,46 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+const PersonComponent = ({ name }) => {
+	return <div>{name}</div>
+}
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-	  <div>debug: {newName}</div>
-      ...
-    </div>
-  )
+const PeopleCollection = ({ persons }) => {
+	return <div>{persons.map(person => <PersonComponent key={person.id} name={person.name} />)}</div>
+}
+
+const App = () => {
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', id: 0 }
+	])
+	const [newName, setNewName] = useState('')
+
+	const addName = (event) => {
+		event.preventDefault();
+
+		const newNameToAdd = newName;
+		setPersons(persons.concat({ name: newNameToAdd, id: persons.length }));
+		setNewName("");
+	}
+
+	const handleNameChange = (event) => {
+		setNewName(event.target.value)
+	}
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addName}>
+				<div>
+					name: <input value={newName}
+						onChange={handleNameChange} />
+				</div>
+
+				<div><button type="submit">add</button></div>
+			</form>
+			<h2>Numbers</h2>
+			<PeopleCollection persons={persons}/>
+		</div>
+	)
 }
 
 export default App
