248135b78d377cb4795098c4c7ddecd399b4c3f8
grml <grml@grml.grml>
2026-05-19 19:15:28 +0800

    feat: added search system

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -16,19 +16,28 @@ const checkDuplicates = (item, array, space) => {
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', id: 0, number: "040-1234567" }
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
 	])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState("");
+	const [searchTermInput, setSearchTermInput] = useState("");
+
+	const personsToShow = searchTermInput === ""
+		? persons
+		: persons.filter(person => person.name.toLowerCase().startsWith(searchTermInput.toLowerCase()));
 
 	const addName = (event) => {
 		event.preventDefault();
 		if (checkDuplicates(newName, persons, "name")) {
-			return alert(`${newName} is already added to phonebook`);
+			alert(`${newName} is already added to phonebook`);
 			setNewName("");
 			setNewNumber("");
+			return;
 		} else {
-			setPersons(persons.concat({ name: newName, id: persons.length, number: newNumber }));
+			setPersons(persons.concat({ name: newName, id: persons.length + 1, number: newNumber }));
 			setNewNumber("");
 			setNewName("");
 		}
@@ -42,9 +51,22 @@ const App = () => {
 		setNewNumber(event.target.value);
 	}
 
+	const handleSearchChange = (event) => {
+		setSearchTermInput(event.target.value);
+	}
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<form>
+				<div>
+					filter shown with
+					<input value={searchTermInput} onChange={handleSearchChange}/>
+				</div>
+			</form>
+
+			<br/>
+
 			<form onSubmit={addName}>
 				<div>
 					name: <input value={newName}
@@ -58,7 +80,7 @@ const App = () => {
 				<div><button type="submit">add</button></div>
 			</form>
 			<h2>Numbers</h2>
-			<PeopleCollection persons={persons}/>
+			<PeopleCollection persons={personsToShow}/>
 		</div>
 	)
 }
